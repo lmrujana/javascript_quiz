5dd11e995882fcb1e3b80ce6cc9e4d5360773afe
Luis Rujana <lmrujanass@gmail.com>
2020-10-20 23:16:44 -0400

    Changed the HTML structure of the quiz cards and addedevent listeners to each card quiz button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Quiz Cards
 var startCard = document.getElementById('start-card');
 var firstQuestion = document.getElementById('first-question');
 var secondQuestion = document.getElementById('second-question');
@@ -10,8 +11,22 @@ var eighthQuestion = document.getElementById('eighth-question');
 var ninthQuestion = document.getElementById('ninth-question');
 var tenthQuestion = document.getElementById('tenth-question');
 
+//Card Button
+var startButton = document.getElementById('start-button');
+var firstButton = document.getElementById('first-button');
+var secondButton = document.getElementById('second-button');
+var thirdButton = document.getElementById('third-button');
+var fourthButton = document.getElementById('fourth-button');
+var fifthButton = document.getElementById('fifth-button');
+var sixthButton = document.getElementById('sixth-button');
+var seventhButton = document.getElementById('seventh-button');
+var eighthButton = document.getElementById('eighth-button');
+var ninthButton = document.getElementById('ninth-button');
+var tenthButton = document.getElementById('tenth-button');
+
+//Hide or show question cards
 startCard.hidden = false;
-firstQuestion.hidden = false;
+firstQuestion.hidden = true;
 secondQuestion.hidden = true;
 thirdQuestion.hidden = true;
 fourthQuestion.hidden = true;
@@ -21,3 +36,57 @@ seventhQuestion.hidden = true;
 eighthQuestion.hidden = true;
 ninthQuestion.hidden = true;
 tenthQuestion.hidden = true;
+
+//Button Interactivity
+startButton.addEventListener('click', function(){
+    firstQuestion.hidden = false;
+    startCard.hidden = true;
+});
+
+firstButton.addEventListener('click', function(){
+    secondQuestion.hidden = false;
+    firstQuestion.hidden = true;
+});
+
+secondButton.addEventListener('click', function(){
+    thirdQuestion.hidden = false;
+    secondQuestion.hidden = true;
+});
+
+thirdButton.addEventListener('click', function(){
+    fourthQuestion.hidden = false;
+    thirdQuestion.hidden = true;
+});
+
+fourthButton.addEventListener('click', function(){
+    fifthQuestion.hidden = false;
+    fourthQuestion.hidden = true;
+});
+
+fifthButton.addEventListener('click', function(){
+    sixthQuestion.hidden = false;
+    fifthQuestion.hidden = true;
+});
+
+sixthButton.addEventListener('click', function(){
+    seventhQuestion.hidden = false;
+    sixthQuestion.hidden = true;
+});
+
+seventhButton.addEventListener('click', function(){
+    eighthQuestion.hidden = false;
+    seventhQuestion.hidden = true;
+});
+
+eighthButton.addEventListener('click', function(){
+    ninthQuestion.hidden = false;
+    eighthQuestion.hidden = true;
+});
+
+ninthButton.addEventListener('click', function(){
+    tenthQuestion.hidden = false;
+    ninthQuestion.hidden = true;
+});
+
+
+
