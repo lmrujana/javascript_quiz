174e3aca9faf722c4643fc1e59ff3474d74be214
Luis Rujana <lmrujanass@gmail.com>
2020-10-22 22:21:09 -0400

    Added functions to the buttons to keep track of the score

--- script.js
+++ script.js
@@ -37,6 +37,9 @@ eighthQuestion.hidden = true;
 ninthQuestion.hidden = true;
 tenthQuestion.hidden = true;
 
+//Score
+var scoreValue = 0;
+
 //Button Interactivity
 startButton.addEventListener('click', function(){
     firstQuestion.hidden = false;
@@ -53,47 +56,192 @@ firstButton.addEventListener('click', function(){
             answerOne = opt.value;
         };
     };
-    console.log(answerOne);
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 secondButton.addEventListener('click', function(){
     thirdQuestion.hidden = false;
     secondQuestion.hidden = true;
+    var secondOpt = document.querySelectorAll('#second-question input[name="answer"]');
+    var answerTwo;
+    for(var opt of secondOpt){
+        if(opt.checked){
+            answerTwo = opt.value;
+        };
+    };
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 thirdButton.addEventListener('click', function(){
     fourthQuestion.hidden = false;
     thirdQuestion.hidden = true;
+    var thirdOpt = document.querySelectorAll('#third-question input[name="answer"]');
+    var answerThree;
+    for(var opt of thirdOpt){
+        if(opt.checked){
+            answerThree = opt.value;
+        };
+    };
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 fourthButton.addEventListener('click', function(){
     fifthQuestion.hidden = false;
     fourthQuestion.hidden = true;
+    var fourthOpt = document.querySelectorAll('#fourth-question input[name="answer"]');
+    var answerFour;
+    for(var opt of fourthOpt){
+        if(opt.checked){
+            answerFour = opt.value;
+        };
+    };
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 fifthButton.addEventListener('click', function(){
     sixthQuestion.hidden = false;
     fifthQuestion.hidden = true;
+    var firstOpt = document.querySelectorAll('#fifth-question input[name="answer"]');
+    var answerOne;
+    for(var opt of firstOpt){
+        if(opt.checked){
+            answerOne = opt.value;
+        };
+    };
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 sixthButton.addEventListener('click', function(){
     seventhQuestion.hidden = false;
     sixthQuestion.hidden = true;
+    var sixthOpt = document.querySelectorAll('#sixth-question input[name="answer"]');
+    var answerSix;
+    for(var opt of sixthOpt){
+        if(opt.checked){
+            answerSix = opt.value;
+        };
+    };
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 seventhButton.addEventListener('click', function(){
     eighthQuestion.hidden = false;
     seventhQuestion.hidden = true;
+    var seventhOpt = document.querySelectorAll('#seventh-question input[name="answer"]');
+    var answerSeven;
+    for(var opt of seventhOpt){
+        if(opt.checked){
+            answerSeven = opt.value;
+        };
+    };
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 eighthButton.addEventListener('click', function(){
     ninthQuestion.hidden = false;
     eighthQuestion.hidden = true;
+    var eighthOpt = document.querySelectorAll('#eighth-question input[name="answer"]');
+    var answerEight;
+    for(var opt of eighthOpt){
+        if(opt.checked){
+            answerEight = opt.value;
+        };
+    };
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 ninthButton.addEventListener('click', function(){
     tenthQuestion.hidden = false;
     ninthQuestion.hidden = true;
+    var ninthOpt = document.querySelectorAll('#ninth-question input[name="answer"]');
+    var answerNine;
+    for(var opt of ninthOpt){
+        if(opt.checked){
+            answerNine = opt.value;
+        };
+    };
+    switch(answerOne){
+        case 'right':
+            scoreValue+=10;
+            break;
+        case 'wrong':
+            if(scoreValue > 10){
+                scoreValue-=10;
+            };
+            break;
+    };
 });
 
 
